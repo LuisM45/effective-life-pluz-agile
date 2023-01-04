4b3b66ddb7c1fdbf45b132d432ec712d5a3785a6
Betsabe Amaguai <betsabe.amaguai@epn.edu.ec>
2023-01-04 10:25:29 -0500

    Paciente placeholder

---
+++ src/main/java/net/epnmag9/effectivelifepluzma/models/Paciente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epnmag9.effectivelifepluzma.models;
+
+import net.epnmag9.effectivelifepluzma.controllers.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Paciente {
+    
+}
