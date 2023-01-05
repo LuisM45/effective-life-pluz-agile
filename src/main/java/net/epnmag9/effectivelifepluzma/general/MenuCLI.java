508e47762865eda8559d3ad98ab00b98521a78a6
LuisM45 <luismateoarg2012@gmail.com>
2023-01-04 22:48:54 -0500

    Register function introduced

--- src/main/java/net/epnmag9/effectivelifepluzma/general/MenuCLI.java
+++ src/main/java/net/epnmag9/effectivelifepluzma/general/MenuCLI.java
@@ -5,6 +5,7 @@
 package net.epnmag9.effectivelifepluzma.general;
 
 import java.util.Scanner;
+import net.epnmag9.effectivelifepluzma.views.PacientesIngresadosVw;
 
 /**
  *
@@ -12,9 +13,11 @@ import java.util.Scanner;
  */
 public class MenuCLI {
     private final Scanner scn = new Scanner(System.in);
+    private final PacientesIngresadosVw pacientesIngresadosVw = PacientesIngresadosVw.createPacientesIngresados();
     
     final static String mainMenu = 
               """
+              1. Ingresar Paciente
               0. Salir
               Ingrese la acción a realizar: """;
     
@@ -24,6 +27,9 @@ public class MenuCLI {
             switch (scn.nextLine()) {
                 case "0":
                     System.exit(0);
+                    break;
+                case "1":
+                    pacientesIngresadosVw.registrarPaciente();
                     break;
                 default:;
             }
