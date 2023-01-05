328f2ca47211da42753d79a56d96c3c67889c9a7
Betsabe Amaguai <betsabe.amaguai@epn.edu.ec>
2023-01-05 12:18:57 -0500

    Test1 added

---
+++ src/test/java/net/epnmag9/effectivelifepluzma/controllers/HabitacionCtrlTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epnmag9.effectivelifepluzma.controllers;
+
+import net.epnmag9.effectivelifepluzma.views.HabitacionVw;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class HabitacionCtrlTest {
+    
+   
+    @Test
+    public void when_paciente_do_put_check_isThere() {
+        HabitacionCtrl habitacionCtrl = new HabitacionCtrl("E");
+        PacienteCtrl pacienteCtrl = new PacienteCtrl("1234567897", "Marco Bolez", "01/01/2001", "A+", "M");
+        
+        habitacionCtrl.putPaciente(pacienteCtrl);
+        assertEquals(pacienteCtrl, habitacionCtrl.peekPaciente());
+    }
+}
