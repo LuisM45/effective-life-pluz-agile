ff29b86f52cf20a3e6749e81db892b838a6bf5dd
LuisM45 <luismateoarg2012@gmail.com>
2023-01-04 22:50:32 -0500

    DatosClinicos placeholders

---
+++ src/main/java/net/epnmag9/effectivelifepluzma/models/DatosClinicosMdl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.epnmag9.effectivelifepluzma.models;
+
+import net.epnmag9.effectivelifepluzma.controllers.*;
+
+/**
+ *
+ * @author luism
+ */
+public class DatosClinicosMdl {
+    
+}
