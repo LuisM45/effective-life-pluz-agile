05b815844082772e23b59b420757a2c5985d0df1
Thomas Tapia <thomas.tapia@epn.edu.ec>
2023-01-05 12:23:59 -0500

    Test DiagnosticoCtrl

---
+++ src/test/java/net/epnmag9/effectivelifepluzma/controllers/DiagnosticoCtrlTest.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.epnmag9.effectivelifepluzma.controllers;
+
+import java.io.Serializable;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author thoma
+ */
+public class DiagnosticoCtrlTest {
+    
+    @Test
+    public void when_datosClinicos_do_create_check_equals(){
+        String diagnostico = "El paciente tiene Amigdalitis";
+        String doctor = "Julio";
+        String fechaDiagnostico = "01/01/2023";
+        DiagnosticoCtrl diagnosticoCtrl = new DiagnosticoCtrl(doctor, diagnostico, fechaDiagnostico);
+        assertEquals(doctor, diagnosticoCtrl.getDiagnostico());
+        assertEquals(doctor, diagnosticoCtrl.getDoctor());
+        assertEquals(fechaDiagnostico, diagnosticoCtrl.getFechaDiagnostico());
+    }
+            
+}
