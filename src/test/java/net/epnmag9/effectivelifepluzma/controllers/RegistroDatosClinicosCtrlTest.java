86b1003ab4ca9f77f39910f319767f83ca9f072b
LuisM45 <luismateoarg2012@gmail.com>
2023-01-04 23:57:33 -0500

    Test1-3 created

--- src/test/java/net/epnmag9/effectivelifepluzma/controllers/RegistroDatosClinicosCtrlTest.java
+++ src/test/java/net/epnmag9/effectivelifepluzma/controllers/RegistroDatosClinicosCtrlTest.java
@@ -16,5 +16,38 @@ import static org.junit.Assert.*;
  * @author luism
  */
 public class RegistroDatosClinicosCtrlTest {
+
+    @Test
+    public void when_datosClinicos_do_register_check_equals() {
+        RegistroDatosClinicosCtrl regDatosClinicosCtrl = new RegistroDatosClinicosCtrl();
+        DatosClinicosCtrl datosClinicosCtrl = new DatosClinicosCtrl(56, 1.5, 56, 76, 38.2, "02/03/2022", "N/A");
+        
+        regDatosClinicosCtrl.pushDatosClinicos(datosClinicosCtrl);
+        assertEquals(datosClinicosCtrl,regDatosClinicosCtrl.getDatosClinicosRecientes());
+    }
     
+    @Test
+    public void when_variousDatosClinicos_do_register_check_historic() {
+        RegistroDatosClinicosCtrl regDatosClinicosCtrl = new RegistroDatosClinicosCtrl();
+        DatosClinicosCtrl datosClinicosCtrl1 = new DatosClinicosCtrl(56, 1.5, 56, 76, 38.2, "02/03/2022", "N/A");
+        DatosClinicosCtrl datosClinicosCtrl2 = new DatosClinicosCtrl(64, 1.2, 59, 66, 38.2, "02/02/2012", "N/A");
+        
+        regDatosClinicosCtrl.pushDatosClinicos(datosClinicosCtrl1);
+        regDatosClinicosCtrl.pushDatosClinicos(datosClinicosCtrl2);
+        assertEquals(datosClinicosCtrl2,regDatosClinicosCtrl.getDatosClinicosRecientes());
+        assertEquals(datosClinicosCtrl1,regDatosClinicosCtrl.getDatosClinicosHistoricos().get(0));
+    }
+    
+    @Test
+    public void when_DatosClinicos_do_update_check_no_historic() {
+        RegistroDatosClinicosCtrl regDatosClinicosCtrl = new RegistroDatosClinicosCtrl();
+        DatosClinicosCtrl datosClinicosCtrl1 = new DatosClinicosCtrl(56, 1.5, 56, 76, 38.2, "02/03/2022", "N/A");
+        DatosClinicosCtrl datosClinicosCtrl2 = new DatosClinicosCtrl(64, 1.2, 59, 66, 38.2, "02/02/2012", "N/A");
+        
+        regDatosClinicosCtrl.pushDatosClinicos(datosClinicosCtrl1);
+        regDatosClinicosCtrl.updateDatosClinicos(datosClinicosCtrl2);
+        assertEquals(datosClinicosCtrl2,regDatosClinicosCtrl.getDatosClinicosRecientes());
+        assertEquals(0,regDatosClinicosCtrl.getDatosClinicosHistoricos().size());
+    }
+
 }
