6ea620d066b094e170a4b703a3a7983eda48d8af
LuisM45 <luismateoarg2012@gmail.com>
2023-01-04 22:48:37 -0500

    RegistroDatosClinicos placeholders

---
+++ src/main/java/net/epnmag9/effectivelifepluzma/views/RegistroDatosClinicosVw.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.epnmag9.effectivelifepluzma.views;
+
+import net.epnmag9.effectivelifepluzma.controllers.*;
+
+/**
+ *
+ * @author luism
+ */
+public class RegistroDatosClinicosVw {
+    
+}
---
+++ src/test/java/net/epnmag9/effectivelifepluzma/controllers/RegistroDatosClinicosCtrlTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package net.epnmag9.effectivelifepluzma.controllers;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author luism
+ */
+public class RegistroDatosClinicosCtrlTest {
+    
+}
